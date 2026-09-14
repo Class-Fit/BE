c2dbf2e9e464488b2174f3790bb8101c8f00aa4d
6zeropk <nemm3623@naver.com>
2026-09-14 18:14:34 +0900

    test: ApiResponse 실패 응답 테스트 추가

--- src/test/java/com/example/classfit/common/ApiResponseTest.java
+++ src/test/java/com/example/classfit/common/ApiResponseTest.java
@@ -1,5 +1,7 @@
 package com.example.classfit.common;
 
+import com.example.classfit.common.exception.CommonErrorCode;
+import com.example.classfit.common.exception.ErrorCode;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,5 +21,26 @@ public class ApiResponseTest {
         assertThat(res.data()).isEqualTo(data);
         assertThat(res.errorCode()).isNull();
         assertThat(res.message()).isNull();
+
+    }
+
+    @Test
+    void createsErrorResponse(){
+
+        ErrorCode errorCode = CommonErrorCode.INVALID_REQUEST;
+
+        // when
+        ApiResponse<Void> response =
+                ApiResponse.failure(
+                        errorCode.getCode(),
+                        errorCode.getMessage()
+                );
+
+        // then
+        assertThat(response.success()).isFalse();
+        assertThat(response.data()).isNull();
+        assertThat(response.errorCode()).isEqualTo(errorCode.getCode());
+        assertThat(response.message()).isEqualTo(errorCode.getMessage());
+
     }
 }
