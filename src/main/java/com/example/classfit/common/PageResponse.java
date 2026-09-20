0472aef2e6c7348f1ece97bdf3f79fb3aeb986c2
6zeropk <nemm3623@naver.com>
2026-09-20 23:31:51 +0900

    feat: PageResponse 변환 메서드 추가

--- src/main/java/com/example/classfit/common/PageResponse.java
+++ src/main/java/com/example/classfit/common/PageResponse.java
@@ -1,5 +1,7 @@
 package com.example.classfit.common;
 
+import org.springframework.data.domain.Page;
+
 import java.util.List;
 
 public record PageResponse<T>(
@@ -11,5 +13,15 @@ public record PageResponse<T>(
         boolean first,
         boolean last
 ){
-
+    public static <T> PageResponse<T> from(Page<T> page) {
+        return new PageResponse<>(
+                page.getContent(),
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalElements(),
+                page.getTotalPages(),
+                page.isFirst(),
+                page.isLast()
+        );
+    }
 }
